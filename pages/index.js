b4a41be992c3106aab7e50393ab7201b1904eb0a
dirtada <47221293+dirtada@users.noreply.github.com>
2025-08-04 09:58:28 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
         alt="Bagr"
         width={700}
         height={500}
-        className="object-contain -mb-12 md:-mb-24"
+        className="object-contain -mb-8 md:-mb-12"
       />
     </div>
   </div>
