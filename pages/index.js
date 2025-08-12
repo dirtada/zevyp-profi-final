7dd20771f199cb64eb5e6512a7dce24612a89c43
dirtada <47221293+dirtada@users.noreply.github.com>
2025-08-12 09:29:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 // pages/index.js
 import Head from "next/head";
+import Link from "next/link";
+
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import { useState, useEffect, useMemo } from "react";
