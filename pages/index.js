c6616db123f516dea1897093a2959746e3d53a15
dirtada <47221293+dirtada@users.noreply.github.com>
2025-12-06 19:03:39 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -463,7 +463,7 @@ export default function Home() {
                       </div>
                       <div className="mt-1 pt-2 border-t-0 border-black/20 text-center"><p className="text-[9px] md:text-[11px] font-bold text-black uppercase tracking-widest">VÝKOPOVÉ PRÁCE • TERÉNNÍ ÚPRAVY • STAVEBNÍ PRÁCE</p></div>
                     </div>
-                    <div className="absolute right-0 bottom-9 w-[150px] md:right-2 md:bottom-6 md:w-[160px] z-30 pointer-events-none drop-shadow-lg">
+                    <div className="absolute right-0 bottom-9 w-[150px] md:right-2 md:bottom-10 md:w-[160px] z-30 pointer-events-none drop-shadow-lg">
                        <img src="/images/flotila_nejlepsi_transparentni.png" alt="Flotila bagrů" className="w-full h-auto object-contain" />
                     </div>
                   </div>
